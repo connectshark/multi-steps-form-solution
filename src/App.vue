92fd585c028720dd29e914e5ec8ca5a25b8e70ad
Chambers <bobhus394@gmail.com>
2023-04-06 16:42:14 +0800

    init project
    rename project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
   <router-view></router-view>
 </template>
-
-
-<style lang="scss">
-#app {
-  font-family: 'Poppins', 'Noto Sans TC', sans-serif;
-  text-align: center;
-}
-</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -4,28 +4,5 @@
   </main>
 </template>
 
-<script>
-export default {
-  setup () {
-    return {
-
-    }
-  }
-}
+<script setup>
 </script>
-
-<style lang="scss" scoped>
-main {
-  height: 100vh;
-  background-color: #111111;
-  display: grid;
-  place-items: center;
-  .title {
-    color:  transparent;
-    font-size: 4rem;
-    background-clip: text;
-    -webkit-background-clip: text;
-    background-image: linear-gradient(to bottom, #5ee7df 0%, #b490ca 100%);
-  }
-}
-</style>
