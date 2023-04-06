81c97b5b152328d70267cd229ba58bb7c2b877c3
Chambers <bobhus394@gmail.com>
2023-04-06 17:06:39 +0800

    set default style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,21 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: 'rgb(var(--color-primary) / <alpha-value>)'
-      }
+    colors: {
+      'primary-marine': 'rgb(var(--primary-marine) / <alpha-value>)',
+      'primary-marine': 'rgb(var(--primary-marine) / <alpha-value>)',
+      'primary-purplish': 'rgb(var(--primary-purplish) / <alpha-value>)',
+      'primary-pastel': 'rgb(var(--primary-pastel) / <alpha-value>)',
+      'primary-light': 'rgb(var(--primary-light) / <alpha-value>)',
+      'primary-strawberry': 'rgb(var(--primary-strawberry) / <alpha-value>)',
+      'neutral-cool': 'rgb(var(--neutral-cool) / <alpha-value>)',
+      'neutral-magnolia': 'rgb(var(--neutral-magnolia) / <alpha-value>)',
+      'neutral-alabaster': 'rgb(var(--neutral-alabaster) / <alpha-value>)',
+      'white': 'rgb(var(--white) / <alpha-value>)'
     },
+    fontFamily: {
+      'ubuntu': ['"Ubuntu"','"Noto Sans TC"', 'sans-serif', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas'],
+    }
   },
   plugins: [],
 }
